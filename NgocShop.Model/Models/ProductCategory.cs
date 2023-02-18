f55355b37a3d0d84f047560c08a1220e98a165e6
Đặng Văn Ngọc <ngocdv.qnh@vnpt.vn>
2023-02-18 23:37:02 +0700

    add domain entities for project

---
+++ NgocShop.Model/Models/ProductCategory.cs
@@ -0,0 +1,32 @@
+﻿using NgocShop.Model.Abstract;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NgocShop.Model.Models
+{
+    [Table("ProductCategories")]
+    public class ProductCategory : Auditable
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        [Required]
+        [MaxLength(256)]
+        public string Name { get; set; }
+        [Required]
+        public string Alias { get; set; }
+        public string Description { get; set; }
+        public int? ParentID { get; set; }
+        public int? DisplayOrder { get; set; }
+        public string Image { get; set; }
+        public bool? HomeFlag { get; set; }
+
+        public virtual IEnumerable<Product> Products { get; set; }
+    }
+}
