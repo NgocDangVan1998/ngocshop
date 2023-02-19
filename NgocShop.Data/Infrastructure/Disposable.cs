a38b36680f2757ebfbbc32137add40d18715705c
Đặng Văn Ngọc <ngocdv.qnh@vnpt.vn>
2023-02-19 14:47:01 +0700

    add entity framework code first for data

---
+++ NgocShop.Data/Infrastructure/Disposable.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NgocShop.Data.Infrastructure
+{
+    public class Disposable : IDisposable
+    {
+        public bool IsDisposed;
+        ~Disposable() 
+        {
+            Dispose(false);
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        private void Dispose(bool disposing)
+        {
+            if (!IsDisposed && disposing) 
+            {
+                DisposeCore();
+            }
+            IsDisposed= true;
+        }
+
+        protected virtual void DisposeCore()
+        {
+
+        }
+    }
+}
