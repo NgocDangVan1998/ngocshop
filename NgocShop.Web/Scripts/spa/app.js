b075c50df4d93c50964d383ca72fb38d0e51c32a
Đặng Văn Ngọc <ngocdv.qnh@vnpt.vn>
2023-03-06 21:40:33 +0700

    service in angularjs

---
+++ NgocShop.Web/Scripts/spa/app.js
@@ -0,0 +1,29 @@
+﻿/// <reference path="../plugins/angular.js" />
+
+var myApp = angular.module('myModule', []);
+
+//Đưa controller vào Module
+myApp.controller("schoolController", schoolController);
+myApp.service('Validator', Validator);
+
+schoolController.$inject = ['$scope','Validator']
+
+function schoolController($scope, Validator) {
+    $scope.checkNumber = function () {
+        $scope.message = Validator.checkNunber($scope.num);
+    }
+    $scope.num = 1;
+}
+
+function Validator($window) {
+    return {
+        checkNunber: checkNunber
+    }
+    function checkNunber(input) {
+        if (input % 2 == 0) {
+            return 'This is even';
+        } else {
+            return 'This is odd';
+        }
+    }
+}
