534ecce2d10983a4d86ea362b4b79d94b5b61672
Đặng Văn Ngọc <ngocdv.qnh@vnpt.vn>
2023-03-10 22:50:35 +0700

    route implement for app

--- NgocShop.Web/app/app.js
+++ NgocShop.Web/app/app.js
@@ -1,1 +1,16 @@
-﻿
+﻿/// <reference path="../assets/admin/libs/angular.js" />
+
+(function () {
+    angular.module('ngocshop', ['ngocshop.product', 'ngocshop.common']).config(config);
+
+    config.$inject = ['$stateProvider', '$urlRouterProvider']
+
+    function config($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('home', {
+            url: "/admin",
+            templateUrl: "/app/components/home/homeView.html",
+            controller: "homeController"
+        });
+        $urlRouterProvider.otherwise('/admin');
+    }
+})();
---
+++ NgocShop.Web/app/shared/modules/ngocshop.common.js
@@ -0,0 +1,3 @@
+﻿(function () {
+    angular.module('ngocshop.common',['ui.router'])
+})();
