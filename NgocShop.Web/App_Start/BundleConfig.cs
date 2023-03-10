0abec04de3c92e4ecf538b2d752dae2dc8672f7b
Đặng Văn Ngọc <ngocdv.qnh@vnpt.vn>
2023-03-10 22:54:09 +0700

    #

--- NgocShop.Web/App_Start/BundleConfig.cs
+++ NgocShop.Web/App_Start/BundleConfig.cs
@@ -8,6 +8,7 @@ namespace NgocShop.Web
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
         }
     }
 }
